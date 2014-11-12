45ab9399a1101cd09e598632c7606a248a977918
annonymouse <annonymouse@example.co.uk>
2014-11-12 02:36:23 +0000

    Use easy_install again

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 
 set -e
 set -x
-apt-get install -y vim git nano screen tightvncserver
-curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-python get-pip.py
+apt-get install -y vim git nano screen tightvncserver python-setuptools
+easy_install pip
 pip install twitter
 
 mv /etc/network/interfaces /etc/network/interfaces.old
